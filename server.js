d2178d77a425a249f0271123ee2fb83c5c2d21c3
ica4798 <tulendikozha@gmail.com>
2024-08-05 13:29:11 +0500

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const { hash } = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
@@ -11,7 +12,13 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
 
+    bcrypt.compare(myPlaintextPassword, hash, (err res) => {
+    console.log(res);
+})
+})
 
 //END_ASYNC
 
